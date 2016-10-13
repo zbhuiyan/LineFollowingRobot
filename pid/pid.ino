f6eb6e4ff0636afc4f971442c918faa517f8db89
adeaver <deaver.andrew@gmail.com>
2016-10-13 19:59:28 -0400

    Scaled bang bang

--- pid/pid.ino
+++ pid/pid.ino
@@ -2,35 +2,29 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
-// Variables for PID control
-double pTerm, iTerm, dTerm;
-double pGain = 2, iGain = 0, dGain = 0;
-double iState = 0, dState = 0;
-const double IMAX = 5, IMIN = -5;
-double pid = 0;
-
 // Initialize the motorshield
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
 // Create the motor objects
-Adafruit_DCMotor *left = AFMS.getMotor(1);
-Adafruit_DCMotor *right = AFMS.getMotor(2);
+Adafruit_DCMotor *left = AFMS.getMotor(3);
+Adafruit_DCMotor *right = AFMS.getMotor(4);
 
 // vRef used in subtractor circuit
-const double REF = 2.80;
+const double REF = 2.88;
 
 // pins
-int reflectancePin = A0;
+int reflectancePin = A2;
 double err = 0;
 
 // debouncing
 unsigned long counter = 0;
-const unsigned long SAMPLING_RATE = 100;
+const unsigned long SAMPLING_RATE = 20;
 
 // speed calculation
-const uint8_t BASE_SPEED = 50;
+int baseSpeed = 30;
+int leftWheelError = 10;
 uint8_t leftSpeed = 1, rightSpeed = 1;
-const int MAX_SCALING = 5;
+int scaling = 12;
 
 void setup() {
   Serial.begin(9600);
@@ -42,80 +36,35 @@ void setup() {
   left->run(FORWARD);
   right->run(FORWARD);
   
-  left->setSpeed(BASE_SPEED);
-  right->setSpeed(BASE_SPEED);
+  left->setSpeed(baseSpeed);
+  right->setSpeed(baseSpeed);
 }
 
 void loop() {
   if(Serial.available()) {
-    iGain = (double) Serial.readStringUntil(',').toFloat();
-    pGain = (double) Serial.readStringUntil(',').toFloat();
-    dGain = (double) Serial.readStringUntil(',').toFloat();
+    scaling = (int) Serial.readStringUntil(',').toFloat();
+    baseSpeed = (int) Serial.readStringUntil(',').toFloat();
+    //sampling = (int) Serial.readStringUntil(',').toFloat();
   }
   
   if(millis() - counter >= SAMPLING_RATE) {
     err = analogRead(reflectancePin) - REF;
-    //pid = updatePID(err);
     scaleSpeeds();
-    left->setSpeed(BASE_SPEED * leftSpeed);
-    right->setSpeed(BASE_SPEED * rightSpeed);
-    Serial.println(leftSpeed);
-    Serial.println(rightSpeed);
+    left->setSpeed((baseSpeed+leftWheelError) + leftSpeed);
+    right->setSpeed(baseSpeed + rightSpeed);
   }
-}
-
-// Calculate the three parts of PID
-double updatePID(double pos) {
- calcProportional(pos);
- calcIntegral(pos);
- calcDerivative(pos);
- return iTerm + pTerm + dTerm;
-}
-
-// calculating the proportional
-void calcProportional(double error) {
- pTerm = pGain * error; 
-}
-
-// calculating the integral
-void calcIntegral(double error) {
-  iState += error;
-  
-  if(iState > IMAX) {
-   iState = IMAX; 
-  }
-  
-  if(iState < IMIN) {
-   iState = IMIN; 
-  }
-  
-  iTerm = iState * iGain;
-}
-
-// calculating the derivative
-void calcDerivative(double pos) {
- dTerm = dGain * (dState - pos);
- dState = pos;
 }
 
 // scaling
 void scaleSpeeds() {
- if(pid > 0) {
-   leftSpeed = (int) (pid/20); 
-   rightSpeed = 1;
- } else if(pid < 0) {
-   rightSpeed = (int) (pid*-1/20);
-   leftSpeed = 1;
+ if(err > 0) {
+   leftSpeed = (int) ((err*10)/50)*scaling; 
+   rightSpeed = (int) ((err*-10)/50)*scaling;
+ } else if(err < 0) {
+   rightSpeed = (int) ((err*-10)/50)*scaling;
+   leftSpeed = (int) ((err*10)/50)*scaling;
  } else {
    leftSpeed = 1;
    rightSpeed = 1;
  } 
- 
- if(leftSpeed > MAX_SCALING) {
-  leftSpeed = MAX_SCALING; 
- }
- 
- if(rightSpeed > MAX_SCALING) {
-  rightSpeed = MAX_SCALING;
- }
 }
